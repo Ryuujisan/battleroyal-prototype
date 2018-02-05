d220f66c7cb12aafb596935195acd0792d172009
Ryuu <sakurai.ryuu@gmail.com>
2018-02-05 16:53:18 +0100

    setup physic class

--- core/src/com/yuri/portablewarrior/MyGdxGame.java
+++ core/src/com/yuri/portablewarrior/MyGdxGame.java
@@ -3,22 +3,37 @@ package com.yuri.portablewarrior;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import physic.Physick;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private Physick physick;
+
+	private Box2DDebugRenderer debugRenderer;
+	private OrthographicCamera camera;
+
 	@Override
 	public void create () {
+		physick = new Physick();
+		camera = new OrthographicCamera();
+		debugRenderer = new Box2DDebugRenderer();
+
+		camera.setToOrtho(false);
 
 	}
+
+
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		debugRenderer.render(physick.getWorld(), camera.combined);
 
 	}
 	
--- core/src/physic/Physick.java
+++ core/src/physic/Physick.java
@@ -1,16 +1,22 @@
 package physic;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Physick {
 
     private World world;
 
+
+
     public Physick() {
         world = new World(new Vector2(0.0f, 0.0f), true);
     }
 
+    public void update(float dt) {
+        world.step(dt, 6,2);
+    }
 
     public World getWorld() {
         return world;
